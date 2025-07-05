4c4185dc98ba6882a7bb2cd092321c226ed2f1de
azraf019 <azraf019@gmail.com>
2025-07-05 10:01:27 +0600

    working on frontend

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import InvoiceUpload from './components/InvoiceUpload';
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Invoice Processor</h1>
+      <InvoiceUpload />
+    </div>
+  );
+}
+
+export default App;
